d5007439de049c713982d8945fd4064b7367ea1b
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-10-18 15:06:00 -0400

    Update ips.py

--- ruqqus/classes/ips.py
+++ ruqqus/classes/ips.py
@@ -8,7 +8,7 @@ class IP(Base):
     __tablename__ = "ips"
 
     id = Column(Integer, primary_key=True)
-    addr = Column(String(64))
+    addr = Column(String(64), index=True)
     reason = Column(String(256), default="")
     banned_by = Column(Integer, ForeignKey("users.id"), default=True)
 
@@ -18,7 +18,7 @@ class Agent(Base):
     __tablename__ = "useragents"
 
     id = Column(Integer, primary_key=True)
-    kwd = Column(String(64))
+    kwd = Column(String(64), index=True)
     reason = Column(String(256), default="")
     banned_by = Column(Boolean, ForeignKey("users.id"), default=True)
     mock = Column(String(256), default="")
