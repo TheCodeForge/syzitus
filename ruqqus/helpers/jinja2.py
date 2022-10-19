1460a04b0a5feb7b6b07468f0184b1669258c175
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-10-19 13:06:31 -0400

    Update jinja2.py

--- ruqqus/helpers/jinja2.py
+++ ruqqus/helpers/jinja2.py
@@ -28,7 +28,7 @@ def total_users(x):
 @cache.memoize(timeout=60 * 60 * 24)
 def source_code(file_name):
 
-    return open(f"./{file_name}, mode="r+").read()
+    return open(f"./{file_name}", mode="r+").read()
 
 
 @app.template_filter("full_link")
