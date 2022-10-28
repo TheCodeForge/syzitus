e5d08548217746e1bbc7beb4857c9fa2d3df3502
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-10-27 21:29:59 -0400

    3.0.11

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -38,7 +38,7 @@ from redis import BlockingConnectionPool, ConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "3.0.10"
+_version = "3.0.11"
 
 app = Flask(__name__,
             template_folder='./templates'
