961b0f108ddcae3b600b1bca748f393a7f800459
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-10-30 18:37:19 -0400

    Update static.py

--- ruqqus/routes/static.py
+++ ruqqus/routes/static.py
@@ -376,7 +376,8 @@ def settings_profile():
 @auth_desired
 def titles():
     return render_template("/help/titles.html",
-                           titles=TITLES)
+                           titles=list(TITLES.values())
+                           )
 
 
 @app.route("/help/badges", methods=["GET"])
