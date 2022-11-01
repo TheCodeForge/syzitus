a61938cb990821b7f184e473e1c5fdac05686b51
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-01 09:35:24 -0400

    Update discord.py

--- syzitus/helpers/discord.py
+++ syzitus/helpers/discord.py
@@ -102,8 +102,7 @@ def add_role(user, role_name):
     role_id = ROLES[role_name]
     url = f"{DISCORD_ENDPOINT}/guilds/{SERVER_ID}/members/{user.discord_id}/roles/{role_id}"
     headers = {"Authorization": f"Bot {BOT_TOKEN}"}
-    x=requests.put(url, headers=headers)
-    debug([x.status_code, x.json()])
+    requests.put(url, headers=headers)
 
 @discord_wrap
 def delete_role(user, role_name):
