eac4eff0474a8cd880850400aca49d10514b782f
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-06 09:57:43 -0500

    Update static.py

--- syzitus/routes/static.py
+++ syzitus/routes/static.py
@@ -52,7 +52,7 @@ def main_css(color, file, n=None):
 
     colors=list(set([TITLES[x].color for x in TITLES]))
 
-    output += "\n".join([f".title-color-{x} \{color: {x};\}" for x in colors])
+    output += "\n".join([f".title-color-{x}"+"{color: #"+x+";}" for x in colors])
 
     resp = Response(output, mimetype='text/css')
     resp.headers.add("Cache-Control", "public")
