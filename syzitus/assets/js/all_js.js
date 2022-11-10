a9dfaefe98bb368c58e0481e7607083f5ff9eeae
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-10 09:00:29 -0500

    move username form submitter to outside form

--- syzitus/assets/js/all_js.js
+++ syzitus/assets/js/all_js.js
@@ -2517,11 +2517,12 @@ function mod_post(url, type, id) {
 
 //post form toast utility function
 var postformtoast = function(x){
+
   var form_id=x.data('form')
   var xhr = new XMLHttpRequest();
   url=$('#'+form_id).prop('action');
-  xhr.open("POST", $('#'+form_id).prop('action'), true);
-  var form = new FormData(document.querySelector('#'+form_id));
+  xhr.open("POST", url, true);
+  var form = new FormData($('#'+form_id)[0]);
   xhr.withCredentials=true;
   xhr.onerror=function() { 
       $('#toast-error .toast-text').text("Something went wrong. Please try again later.");
