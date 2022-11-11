df928ba8894a59045a28ed7be99a64257378764e
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-11 17:23:23 -0500

    3.1.6

--- syzitus/__main__.py
+++ syzitus/__main__.py
@@ -38,7 +38,7 @@ from redis import BlockingConnectionPool, ConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "3.1.5"
+_version = "3.1.6"
 
 app = Flask(__name__,
             template_folder='./templates'
