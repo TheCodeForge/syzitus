74881a2aec6ff6951d41090fabc6375f3cf235e3
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-12 14:42:39 -0500

    Update outside_embed.js

--- syzitus/assets/js/outside_embed.js
+++ syzitus/assets/js/outside_embed.js
@@ -3,7 +3,7 @@
 var frames = document.getElementsByClassName('syzitus-embed');
 for (let i=0; i<frames.length; i++){
   frame=frames[i];
-  frame.addEventListener('message' function(event){
+  frame.addEventListener('message', function(event){
     var data= event.data;
     if (data.height) {
       frame.height=data.height;
