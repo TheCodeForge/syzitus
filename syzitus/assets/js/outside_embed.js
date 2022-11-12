9a044eaeb87007b28ffe335e4839f6f5d8cc806f
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-12 14:49:56 -0500

    more embed fixing?

--- syzitus/assets/js/outside_embed.js
+++ syzitus/assets/js/outside_embed.js
@@ -1,12 +1,7 @@
 //no jquery allowed here.... sad!
-
-var frames = document.getElementsByClassName('syzitus-embed');
-for (let i=0; i<frames.length; i++){
-  frame=frames[i];
-  frame.addEventListener('message', function(event){
-    var data= event.data;
-    if (data.height) {
-      frame.height=data.height;
-    }
-  })
-}
+window.addEventListener('message', function(event){
+  var data = event.data;
+  if (data.height && data.frame) {
+    document.getElementById(data.frame).height=data.height;
+  }
+})
