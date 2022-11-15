f16bc3809bba6cc15215a02d5148c8a6b9c95040
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-15 17:30:24 -0500

    color updates

--- syzitus/__main__.py
+++ syzitus/__main__.py
@@ -38,7 +38,7 @@ from redis import BlockingConnectionPool, ConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "3.1.24"
+_version = "3.1.25"
 
 app = Flask(__name__,
             template_folder='./templates'
