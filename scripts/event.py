43e309a226930b4938b5e1ff1d136d1c918f7c14
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-11-26 23:07:29 -0500

    Update event.py

--- scripts/event.py
+++ scripts/event.py
@@ -11,78 +11,78 @@ hwen_id = db.query(Board).filter_by(name="Team_Halloween").first().id
 
 #assemble list of Christmas users
 xmas_users = db.query(User).filter(
-	User.id.op('%')(2)==1,
-	or_(
-		User.id.in_(
-			select(Submission.author_id).filter(Submission.board_id==xmas_id)
-			),
-		User.id.in_(
-			select(Comment.author_id).filter(
-				Comment.parent_submission.in_(
-					select(Submission.id).filter(Submission.board_id==xmas_id)
-					)
-				)
-			)
-		)
-	)
+    User.id.op('%')(2)==1,
+    or_(
+        User.id.in_(
+            select(Submission.author_id).filter(Submission.board_id==xmas_id)
+            ),
+        User.id.in_(
+            select(Comment.author_id).filter(
+                Comment.parent_submission.in_(
+                    select(Submission.id).filter(Submission.board_id==xmas_id)
+                    )
+                )
+            )
+        )
+    )
 
 print(list(xmas_users))
 
 #assemble list of Christmas users
 hwen_users = db.query(User).filter(
-	User.id.op('%')(2)==0,
-	or_(
-		User.id.in_(
-			select(Submission.author_id).filter(Submission.board_id==hwen_id)
-			),
-		User.id.in_(
-			select(Comment.author_id).filter(
-				Comment.parent_submission.in_(
-					select(Submission.id).filter(Submission.board_id==hwen_id)
-					)
-				)
-			)
-		)
-	)
+    User.id.op('%')(2)==0,
+    or_(
+        User.id.in_(
+            select(Submission.author_id).filter(Submission.board_id==hwen_id)
+            ),
+        User.id.in_(
+            select(Comment.author_id).filter(
+                Comment.parent_submission.in_(
+                    select(Submission.id).filter(Submission.board_id==hwen_id)
+                    )
+                )
+            )
+        )
+    )
 
 print(list(hwen_users))
 
 
 for user in xmas_users:
-	if not user.has_badge(18):
-		new_badge = Badge(
-			user_id=user.id,
+    if not user.has_badge(18):
+        new_badge = Badge(
+            user_id=user.id,
             badge_id=18,
             created_utc=int(time.time())
             )
 
-		g.db.add(new_badge)
-		g.db.commit()
+        g.db.add(new_badge)
+        g.db.commit()
 
-	  	text = f"""
+        text = f"""
 You have recieved the following badge for your participation in the successfull defense of Christmas from the Halloween invasion:
 \n\n![]({new_badge.path})
 \n\n{new_badge.name}
 """
 
-    	send_notification(user, text)
+        send_notification(user, text)
 
 for user in hwen_users:
-	if not user.has_badge(17):
-		new_badge = Badge(
-			user_id=user.id,
+    if not user.has_badge(17):
+        new_badge = Badge(
+            user_id=user.id,
             badge_id=17,
             created_utc=int(time.time())
             )
 
-		g.db.add(new_badge)
-		g.db.commit()
+        g.db.add(new_badge)
+        g.db.commit()
 
-		text = f"""
+        text = f"""
 You have recieved the following badge for your particpation in Halloween's ill-fated invasion of Christmas:
 \n\n![]({new_badge.path})
 \n\n{new_badge.name}
 """
 
-    	send_notification(user, text)
+        send_notification(user, text)
 
