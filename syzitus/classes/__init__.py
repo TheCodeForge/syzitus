205369a58e0ee80396fd08df2bb29745bf47a150
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-12-02 11:09:22 -0500

    Update __init__.py

--- syzitus/classes/__init__.py
+++ syzitus/classes/__init__.py
@@ -3,6 +3,7 @@ from .badges import BadgeDef, Badge
 from .badwords import BadWord
 from .boards import Board
 from .board_relationships import ModRelationship, BanRelationship, ContributorRelationship, PostRelationship, BoardBlock
+from .categories import CATEGORIES, SUBCAT_DATA
 from .clients import OauthApp, ClientAuth
 from .comment import Comment, CommentAux
 from .custom_errors import PaymentRequired, DatabaseOverload
@@ -14,7 +15,7 @@ from .mod_logs import ModAction
 from .paypal import PayPalClient, PayPalTxn, PromoCode, AwardRelationship
 from .submission import Submission, SubmissionAux
 from .subscriptions import Subscription, Follow
-from .titles import Title
+from .titles import Title, TITLES
 from .user import User
 from .userblock import UserBlock
 from .votes import Vote, CommentVote
