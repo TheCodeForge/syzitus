7118b51dd8c8fe4156757706655a6a5898804788
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-12-03 00:56:48 -0500

    Update get.py

--- syzitus/helpers/get.py
+++ syzitus/helpers/get.py
@@ -1,8 +1,8 @@
 from .base36 import base36encode, base36decode
 from syzitus.classes import *
 from flask import g, abort
-from sqlalchemy import select, or_, and_, Load
-from sqlalchemy.orm import aliased, joinedload, lazyload
+from sqlalchemy import select, or_, and_
+from sqlalchemy.orm import aliased, joinedload, lazyload, Load
 from urllib.parse import urlparse
 from syzitus.__main__ import debug
 
