36fec6354164ff5165e2f86e8c025f4e7df029de
CodeForge <92867553+TheCodeForge@users.noreply.github.com>
2022-12-03 10:46:35 -0500

    Update comments.py

--- syzitus/routes/comments.py
+++ syzitus/routes/comments.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import secrets
 import threading
 from os import environ
-from flask import g, session, abort, render_template, jsonify, make_response
+from flask import g, session, abort, render_template, jsonify, make_response, redirect
 
 from syzitus.helpers.wrappers import *
 from syzitus.helpers.base36 import base36encode, base36decode
