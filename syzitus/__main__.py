59b20956a60f08695c08c1a7f3b02ea2ffa29d88
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-12-07 09:21:16 -0500

    add auto-recoloring ruqqie

--- syzitus/__main__.py
+++ syzitus/__main__.py
@@ -184,6 +184,7 @@ app.config["COINS_REQUIRED_CHANGE_USERNAME"]=int(environ.get("COINS_REQUIRED_CHA
 app.config["COOLDOWN_DAYS_CHANGE_USERNAME"]=int(environ.get("COOLDOWN_DAYS_CHANGE_USERNAME", 7))
 
 #precompute logo urls
+app.config["IMG_URL_MASCOT"] = f"/mascot/{app.config['COLOR_PRIMARY'].lower()}"
 app.config["IMG_URL_LOGO_WHITE"] = f"/logo/white/{app.config['COLOR_PRIMARY'].lower()}/{app.config['SITE_NAME'][0].lower()}"
 app.config["IMG_URL_LOGO_MAIN"] = f"/logo/main/{app.config['COLOR_PRIMARY'].lower()}/{app.config['SITE_NAME'][0].lower()}"
 app.config["IMG_URL_JUMBOTRON"] = f"/logo/jumbotron/{app.config['COLOR_PRIMARY'].lower()}/{app.config['SITE_NAME'][0].lower()}"
