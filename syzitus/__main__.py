928c877dc5ace7acdf94d349a4080cc2b83471a3
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-12-14 10:18:32 -0500

    Update __main__.py

--- syzitus/__main__.py
+++ syzitus/__main__.py
@@ -479,25 +479,10 @@ except ProgrammingError:
 
 
 #this function came from stackoverflow
-def get_size(obj, seen=None):
-    """Recursively finds size of objects"""
-    size = getsizeof(obj)
-    if seen is None:
-        seen = set()
-    obj_id = id(obj)
-    if obj_id in seen:
-        return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, '__dict__'):
-        size += get_size(obj.__dict__, seen)
-    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
-    return size
+import resource
+def using(point=""):
+    usage=resource.getrusage(resource.RUSAGE_SELF)
+    return f"{point}: usertime={usage[0]} systime={usage[1]} mem={usage[2]/1024.0} mb"
 
 with app.test_request_context('/'):
-    debug(f"Loaded app size: {get_size(app)}")
+    debug(usage("Fully loaded"))
