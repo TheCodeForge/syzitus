a010396b2b09e769618327716dfdc85b59dfaf14
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-03 22:58:01 -0500

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -1264,10 +1264,10 @@ class User(Base, standard_mixin, age_mixin):
                 g.db.add(application)
 
 
-        self.has_profile = False
-        self.profile_nonce+=1
-        self.has_banner = False
-        self.banner_nonce+=1
+            self.has_profile = False
+            self.profile_nonce+=1
+            self.has_banner = False
+            self.banner_nonce+=1
         
         g.db.add(self)
         g.db.commit()
