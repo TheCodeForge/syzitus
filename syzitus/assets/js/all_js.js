74b97fba86eaf5eea57908feb381f94db295e8f5
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-07 14:37:59 -0500

    Update all_js.js

--- syzitus/assets/js/all_js.js
+++ syzitus/assets/js/all_js.js
@@ -183,6 +183,7 @@ $('.btn-remove-comment').click(function () {
   var url;
   url="/api/ban_comment/"+$(this).data('comment-id');
   callback=function(xhr){
+    console.log('remove callback')
     $("#comment-"+$(this).data('comment-id')+"-only").addClass("banned");
     $('.btn-remove-comment-'+$(this).data('comment-id')).addClass("d-none")
     $('.btn-approve-comment-'+$(this).data('comment-id')).removeClass("d-none")
@@ -197,6 +198,7 @@ $('.btn-approve-comment').click(function () {
 
   var url="/api/unban_comment/"+$(this).data('comment-id');
   callback=function(xhr){
+    console.log('approve callback')
     $("#comment-"+$(this).data('comment-id')+"-only").removeClass("banned");
     $('.btn-remove-comment-'+$(this).data('comment-id')).removeClass("d-none")
     $('.btn-approve-comment-'+$(this).data('comment-id')).addClass("d-none")
