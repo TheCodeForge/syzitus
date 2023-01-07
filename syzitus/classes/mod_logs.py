a0d7a30af2b4b2c93f5c14b1415620ef129ff64a
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-07 14:20:08 -0500

    Update mod_logs.py

--- syzitus/classes/mod_logs.py
+++ syzitus/classes/mod_logs.py
@@ -83,7 +83,7 @@ class ModAction(Base, standard_mixin, age_mixin):
         elif self.target_post:
             return f'<a href="{self.target_post.permalink}">{self.target_post.title}</a>'
         elif self.target_comment:
-            return f'<a href="{self.target_comment.permalink}">comment</a>'
+            return f'<a href="{self.target_comment.permalink}">comment {self.target_comment.base36id}</a>'
 
         else:
             return ''
