34a79411caac1b9e43afb4fd759230779e2862e0
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-09 13:29:17 -0500

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -1502,7 +1502,7 @@ class User(Base, standard_mixin, age_mixin):
 
     @property
     def can_upload_comment_image(self):
-        return self.has_premium and (request.headers.get("cf-ipcountry")!="T1" or self.is_activated)
+        return request.headers.get("cf-ipcountry") != "T1" #self.has_premium and (request.headers.get("cf-ipcountry")!="T1" or self.is_activated)
 
     @property
     def can_change_name(self):
