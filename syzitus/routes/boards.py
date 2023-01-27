bf8e2a011651b1aedc45bb067c6b9dd8e53d8e34
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-27 17:42:56 -0500

    Update boards.py

--- syzitus/routes/boards.py
+++ syzitus/routes/boards.py
@@ -2277,7 +2277,6 @@ def board_mod_perms_change(guildname, board):
     u_mod.perm_full         = bool(request.form.get("perm_full"         , False))
     u_mod.perm_access       = bool(request.form.get("perm_access"       , False))
     u_mod.perm_appearance   = bool(request.form.get("perm_appearance"   , False))
-    u_mod.perm_chat         = bool(request.form.get("perm_chat"       , False))
     u_mod.perm_config       = bool(request.form.get("perm_config"       , False))
     u_mod.perm_content      = bool(request.form.get("perm_content"      , False))
 
