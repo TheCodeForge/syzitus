f91b978145278129ca7d7c74207e486215e9e1cb
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-27 17:36:10 -0500

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -424,9 +424,6 @@ class User(Base, standard_mixin, age_mixin):
         if g.user and g.user.hide_offensive and g.user.id!=self.id:
             submissions = submissions.filter_by(is_offensive=False)
 
-        if g.user and g.user.hide_bot:
-            submissions = submissions.filter_by(is_bot=False)
-
         if not (g.user and g.user.admin_level >= 3):
             submissions = submissions.filter_by(deleted_utc=0, is_banned=False).join(Submission.board).filter(Board.is_banned==False)
 
@@ -490,9 +487,6 @@ class User(Base, standard_mixin, age_mixin):
 
         if g.user and g.user.hide_offensive and g.user.id != self.id:
             comments = comments.filter(Comment.is_offensive == False)
-
-        if g.user and g.user.hide_bot:
-            comments = comments.filter(Comment.is_bot == False)
 
         if g.user and not g.user.show_nsfl:
             comments = comments.filter(Submission.is_nsfl == False)
