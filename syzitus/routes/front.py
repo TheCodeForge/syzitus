612fc2def9cb66acb07ec0920ed53f9de63b5509
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-27 16:03:39 -0500

    Update front.py

--- syzitus/routes/front.py
+++ syzitus/routes/front.py
@@ -1,6 +1,6 @@
 from flask import g, session, abort, render_template, jsonify, make_response, redirect
 from sqlalchemy import select, and_, or_
-from sqlalchemy.orm import lazyload, contains_eager, Load
+from sqlalchemy.orm import lazyload, contains_eager, Load, load_only
 from random import randint
 
 from syzitus.helpers.wrappers import *
