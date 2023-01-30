de7ce5c73c86ce9791b0a4bcdc377e048d375f75
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-01-30 08:31:58 -0500

    Update settings.py

--- syzitus/routes/settings.py
+++ syzitus/routes/settings.py
@@ -102,7 +102,7 @@ def settings_profile_post():
         
         #seo profile spam
         sus_tags=BeatuifulSOup(bio_html).find('a')
-        if g.timestamp-g.user.created_utc < 60*60*24 and not g.user.post_count and not g.user.comment_count and BeautifulSoup(bio_html).find('a'):
+        if g.timestamp-g.user.created_utc < 60*60*24*2 and not g.user.post_count and not g.user.comment_count and BeautifulSoup(bio_html).find('a'):
             g.user.ban(reason="seo spam")
         
         return jsonify({"message":"Your bio has been updated."})
