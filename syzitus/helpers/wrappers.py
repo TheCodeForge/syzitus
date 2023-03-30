a1c0a6fe01dea0c776ddd3faf72b3b026ea9bbb6
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-03-30 18:06:51 -0400

    Update wrappers.py

--- syzitus/helpers/wrappers.py
+++ syzitus/helpers/wrappers.py
@@ -578,3 +578,22 @@ def cf_cache(f):
     wrapper.__name__=f.__name__
     wrapper.__doc__ = f.__doc__
     return wrapper
+
+def per_page(f):
+
+    #wrapper that adjusts the per-page qty depending on premium status and user settings
+
+    def wrapper(*args, **kwargs):
+
+        if g.user and g.user.has_premium:
+            g.per_page=max(25, g.user.per_page_preference)
+        else:
+            g.per_page=25
+
+        resp=f(*args, **kwargs)
+
+        return resp
+
+    wrapper.__name__=f.__name__
+    wrapper.__doc__ = f.__doc__
+    return wrapper
