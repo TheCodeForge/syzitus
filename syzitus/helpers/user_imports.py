8aa0631606d57cd4e4f95e0cf4065dbcf52cd4c9
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-03-30 20:09:37 -0400

    Update user_imports.py

--- syzitus/helpers/user_imports.py
+++ syzitus/helpers/user_imports.py
@@ -28,22 +28,3 @@ def send_notif(user, text):
                          user_id=user.id)
     g.db.add(notif)
     g.db.commit()
-
-def per_page(f):
-
-    #wrapper that adjusts the per-page qty depending on premium status and user settings
-
-    def wrapper(*args, **kwargs):
-
-        if g.user and g.user.has_premium:
-            g.per_page=max(25, g.user.per_page_preference)
-        else:
-            g.per_page=25
-
-        resp=f(*args, **kwargs)
-
-        return resp
-
-    wrapper.__name__=f.__name__
-    wrapper.__doc__ = f.__doc__
-    return wrapper
