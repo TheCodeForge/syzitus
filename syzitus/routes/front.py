b9790988544f5f307d03bd27a9765a63df44c26c
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-04 10:58:40 -0400

    Update front.py

--- syzitus/routes/front.py
+++ syzitus/routes/front.py
@@ -994,7 +994,7 @@ Optional query parameters:
 
     posts = get_posts(ids)
 
-    return {'html': lambda: render_template("subscriptions.html",
+    return {'html': lambda: render_template("home.html",
                                             listing=posts,
                                             next_exists=next_exists,
                                             page=page),
