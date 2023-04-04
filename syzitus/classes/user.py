b29719c59ee90d113c9c874453d88be86a56f9e7
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-04 16:10:20 -0400

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -458,11 +458,13 @@ class User(Base, standard_mixin, age_mixin):
 
         #here's part 2 of the algorithm core
         #join against the ranking subquery, sort by that rank
+        #Newer content breaks ties
         posts=posts.join(
             ranks,
             Submission.id==ranks.c.submission_id
             ).order_by(
-            ranks.c.rank.desc()
+            ranks.c.rank.desc(),
+            Submission.created_utc.desc()
             ).offset(per_page * (page - 1)).limit(per_page+1).all()
 
         return [x.id for x in posts]
