03c06d505a5424cba0ac8b10b2eeb625e5dd70fa
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-04 11:13:27 -0400

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -327,22 +327,15 @@ class User(Base, standard_mixin, age_mixin):
 
         #this is the meat - filter by posts upvoted by people who've upvoted the same things you've upvoted
         #really crude version - no discounting of auto-self-upvote for example
+
+
         posts=posts.filter(
             Submission.id.in_(
                 select(Vote.submission_id).filter(
-                    Vote.vote_type==1,
                     Vote.user_id.in_(
-                        select(User.id).filter(
-                            User.id.in_(
-                                select(Vote.user_id).filter(
-                                    Vote.vote_type==1,
-                                    Vote.submission_id.in_(
-                                        select(Vote.submission_id).filter(
-                                            Vote.vote_type==1,
-                                            Vote.user_id==g.user.id
-                                            )
-                                        )
-                                    )
+                        select(Vote.user_id).filter(
+                            Vote.submission_id.in_(
+                                select(Vote.submission_id).filter(Vote.vote_type==1, Vote.user_id==g.user.id)
                                 )
                             )
                         )
