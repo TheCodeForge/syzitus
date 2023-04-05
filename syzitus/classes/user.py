84ef3f1e0699432ed21cce2da6710bf5ab4c92de
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-05 10:22:50 -0400

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -433,7 +433,7 @@ class User(Base, standard_mixin, age_mixin):
         #here's part 2 of the algorithm core
         #develop some ranking subqueries, join them onto existing posts query
 
-        posts_subq=posts.limit(500).subquery()
+        posts_subq=posts.limit(200).subquery()
 
         #Votes subquery - the only votes we care about are those from users who co-voted the user's last 100 upvotes
 
