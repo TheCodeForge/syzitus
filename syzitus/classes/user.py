7c9e9eae583c369e4da6959c093b881967a67052
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-04 23:08:14 -0400

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -437,7 +437,7 @@ class User(Base, standard_mixin, age_mixin):
 
         #Votes subquery - the only votes we care about are those from users who co-voted the user's last 100 upvotes
 
-        votes=select(Vote).filter(
+        votes=select(Vote).optiosn(lazyload('*')).filter(
             Vote.vote_type==1,
             Vote.user_id.in_(
                 select(Vote.user_id).filter(
@@ -476,7 +476,7 @@ class User(Base, standard_mixin, age_mixin):
                 ).label('guild_penalty')
             ).group_by(initial.c.submission_id, initial.c.rank, Submission.author_id, Submission.board_id).subquery()
 
-        posts=posts.join(
+        posts=posts.reset_joinpoint().join(
             scoring_subq, 
             Submission.id==scoring_subq.c.submission_id
             )
