1bdcfd538f291a93a5914a701c10231989f93e29
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-05 08:57:08 -0400

    Update user.py

--- syzitus/classes/user.py
+++ syzitus/classes/user.py
@@ -487,7 +487,8 @@ class User(Base, standard_mixin, age_mixin):
             ).order_by(
             # Submission.score_best.desc()
             # scores.c.rank.desc()
-            (scores.c.rank - scores.c.user_penalty - scores.c.board_penalty*2 - scores.c.age_penalty).desc()
+            (scores.c.rank - scores.c.user_penalty - scores.c.board_penalty*2 - scores.c.age_penalty).desc(),
+            scores.c.created_utc.desc()
             )
     
         post_ids=post_ids.offset(per_page * (page - 1)).limit(per_page+1).all()
