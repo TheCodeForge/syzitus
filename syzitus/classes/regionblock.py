29456f70930bf1e5d36de48b953c8d28413c9843
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-20 16:54:00 -0400

    Create regionblock.py

---
+++ syzitus/classes/regionblock.py
@@ -0,0 +1,12 @@
+from sqlalchemy import *
+
+from syzitus.__main__ import app, Base
+
+class RegionBlock(Base):
+
+    __tablename__="regionblocks"
+
+    id      =Column(Integer, primary_key=True)
+    country =Column(Char(2))
+    post_id =Column(Integer, ForeignKey("submissions.id"))
+    board_id=Column(integer, ForeignKey("boards.id"))
