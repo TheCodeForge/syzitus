93f3601f80ec7a55b4ac4cb9c060b08bcdc86525
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-04-20 17:07:05 -0400

    Update regionblock.py

--- syzitus/classes/regionblock.py
+++ syzitus/classes/regionblock.py
@@ -7,6 +7,7 @@ class RegionBlock(Base):
     __tablename__="regionblocks"
 
     id      =Column(Integer, primary_key=True)
-    country =Column(Char(2))
+    country =Column(String(2))
     post_id =Column(Integer, ForeignKey("submissions.id"))
     board_id=Column(integer, ForeignKey("boards.id"))
+    note    =Column(String(512))
